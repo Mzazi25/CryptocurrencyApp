22f6eff7bd95d25193565903e4617f2f2feb24ae
Mzazi25 <langat.caleb25@gmail.com>
2022-08-18 21:38:57 +0300

    Implemented CoinDetailViewModel

---
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_detail/CoinDetailState.kt
@@ -0,0 +1,10 @@
+package com.example.cryptocurrencyapp.presentation.coin_detail
+
+import com.example.cryptocurrencyapp.domain.models.Coin
+import com.example.cryptocurrencyapp.domain.models.CoinDetails
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetails? = null,
+    val error: String = ""
+)
