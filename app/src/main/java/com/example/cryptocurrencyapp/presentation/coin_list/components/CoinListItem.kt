a7ac0ab2ecef717e00c4cb1d7271502006a3963e
Mzazi25 <langat.caleb25@gmail.com>
2022-08-22 09:34:16 +0300

    Refactored CoinListItem

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
@@ -1,10 +1,27 @@
 package com.example.cryptocurrencyapp.presentation.coin_list.components
 
+import android.graphics.Paint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambdaInstance
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cryptocurrencyapp.domain.models.Coin
+import com.example.cryptocurrencyapp.presentation.coin_list.CoinListState
 
 @Composable
 fun CoinListItem(
@@ -12,8 +29,37 @@ fun CoinListItem(
     onItemClick:(Coin) ->Unit
 ) {
     Row(
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(coin) }
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
     ){
+        Text(
+            text = "${coin.rank}.${coin.name}.(${coin.symbol})",
+            style = MaterialTheme.typography.body1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = if (coin.is_active) "Active" else "Inactive",
+            color = if (coin.is_active) Color.Green else Color.Red,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.align(CenterVertically)
+        )
 
     }
+}
+
+@Preview
+@Composable
+fun CoinListPreview() {
+    CoinListItem(coin =Coin(
+        id = "Btc",
+        is_active = true,
+        name = "Bitcoin",
+        rank = 1,
+        symbol = "BTC"
+    ), onItemClick = {})
 }
