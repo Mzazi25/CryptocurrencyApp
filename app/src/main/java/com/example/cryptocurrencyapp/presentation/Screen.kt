e9879a79b76b8704182559665f0465840a707c3b
Mzazi25 <langat.caleb25@gmail.com>
2022-09-17 08:18:50 +0300

    Updated screen class with details screen arguments

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/Screen.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/Screen.kt
@@ -2,5 +2,9 @@ package com.example.cryptocurrencyapp.presentation
 
 sealed class Screen(val route: String){
     object CoinListScreen:Screen("coin_list_screen")
-    object CoinDetailScreen:Screen("coin_detail_screen")
+    object CoinDetailScreen:Screen("coin_detail_screen/{coinId}"){
+        fun passHeroId(coinId:Int): String{
+            return "detail_screen/$coinId"
+        }
+    }
 }
